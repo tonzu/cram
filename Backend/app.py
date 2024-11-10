35b1b18084f0c81470233e9360282bb6f28f302b
ewang02 <ewang02@berkeley.edu>
2024-11-10 04:58:44 -0800

    Update app.py
    Adds upload to S3 functionality

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 from models import *
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    s3_upload()
+    return render_template("upload.html")
 
 if __name__ == '__main__':
     app.run()
