8a050fcbb265155350711c01817382f240417866
pravinrajah14 <pravin.rajah@gmail.com>
2024-11-22 15:14:06 -0800

    Fixed controls

--- frontend/src/components/SpeedControl.js
+++ frontend/src/components/SpeedControl.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import './PlayerControls.css';
+import './SpeedControl.css';
 
-function PlayerControls({ isPlaying, setIsPlaying }) {
-  const handlePlayPause = () => setIsPlaying(!isPlaying);
+function SpeedControl({ wordSpeed, setWordSpeed, eyeTracking, setEyeTracking }) {
+  const handleSpeedChange = (e) => {
+    setWordSpeed(e.target.value * 5);
+  };
 
   return (
-    <div className="player-controls">
-      <button>⏪</button>
-      <button onClick={handlePlayPause}>{isPlaying ? '⏸' : '▶'}</button>
-      <button>⏩</button>
+    <div className="speed-control">
+      <span>Word speed</span>
+      <input
+        type="range"
+        min="1"
+        max="150"
+        value={wordSpeed / 5}
+        onChange={handleSpeedChange}
+      />
+      <span>{wordSpeed}</span>
+      <label>
+        <input 
+          type="checkbox" 
+          checked={eyeTracking} 
+          onChange={() => setEyeTracking(!eyeTracking)} 
+        /> 
+        Eye-Tracking
+      </label>
     </div>
   );
 }
 
-export default PlayerControls;
+export default SpeedControl;
