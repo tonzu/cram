b4b9c584c3258197c9d893c8757a0262038b1c7f
Cyrus Wise <catstinks@gmail.com>
2024-11-24 14:57:18 -0800

    Added getters for email & UID, and email verification for new accounts

--- frontend/src/TestPage.js
+++ frontend/src/TestPage.js
@@ -5,7 +5,7 @@ import AuthTest from "./Auth-test"; // Import the AuthTest component
 const TestPage = () => {
   return (
     <div>
-      <h1>Test Page</h1>
+      <h1>CRAM</h1>
       <AuthTest /> {/* Render the AuthTest component here */}
     </div>
   );
