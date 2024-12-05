ef1556216b2b98cc9d4ee2c9d7bb5a1f228d3533
ewang02 <ewang02@berkeley.edu>
2024-12-05 11:17:43 -0800

    Delete Backend/db_handler.py

--- Backend/db_handler.py
+++
@@ -1,7 +0,0 @@
-from app import db
-from models import Document
-
-def add_document(user_id, filename, content):
-    new_document = Document(user_id=user_id, filename=filename, content=content)
-    db.session.add(new_document)
-    db.session.commit()
