73a3bdc90311e0f7b4c24ac7d8eefe63007cea81
ewang02 <ewang02@berkeley.edu>
2024-12-05 11:18:17 -0800

    Delete Backend/models.py

--- Backend/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-class Document(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    filename = db.Column(db.String(255))
-    uploaded_at = db.Column(db.DateTime, server_default=db.func.now())
-    content = db.Column(db.Text)
